d88075e347fd6389690ff17145b672aa03e35d8d
Ben Taylor <ben.taylor.uk@gmail.com>
2010-11-18 14:57:18 +0000

    Added Should assertion library. Started PayPalNvp.

---
+++ src/Kurejito/Gateways/PayPalNvp/PayPalNvpPaymentGateway.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Kurejito.Payments;
+
+namespace Kurejito.Gateways.PayPalNvp
+{
+    public class PayPalNvpPaymentGateway : IPurchaseGateway
+    {
+        private readonly PayPalCredentials _payPalCredentials;
+
+        public PayPalNvpPaymentGateway(PayPalCredentials payPalCredentials)
+        {
+            if (payPalCredentials == null) throw new ArgumentNullException("payPalCredentials");
+            _payPalCredentials = payPalCredentials;
+        }
+
+        public PaymentResponse Purchase(string merchantReference, decimal amount, string currency, BankCard card)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PayPalCredentials
+    {
+        public PayPalCredentials(string username, string password, string signature)
+        {
+            if (username == null) throw new ArgumentNullException("username");
+            if (password == null) throw new ArgumentNullException("password");
+            if (signature == null) throw new ArgumentNullException("signature");
+            Username = username;
+            Password = password;
+            Signature = signature;
+        }
+
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Signature { get; private set; }
+    }
+}
