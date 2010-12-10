e0966a3af13b87350cc2cb7295089e23f3e14906
Ben Taylor <ben.taylor.uk@gmail.com>
2010-12-10 17:57:36 +0000

    * Moved direct payment api errors from csv to xls so we can use the ExcelData attribute on a unit test, to check all possible error code handling. * Had to set the ProviderTests assembly to target 32 bit CPU as there is no 64 bit version of the Jet Oledb provider.

--- src/Kurejito.ProviderTests/PayPal/PayPalPurchaseTests.cs
+++ src/Kurejito.ProviderTests/PayPal/PayPalPurchaseTests.cs
@@ -3,7 +3,7 @@ using Kurejito.Gateways.PayPal;
 using Kurejito.Gateways.PayPal.DirectPayment;
 using Kurejito.Payments;
 using Kurejito.Transport;
-using Xunit;
+using Xunit.Extensions;
 
 namespace Kurejito.ProviderTests.PayPal {
     public class PayPalPurchaseTests : PurchaseGatewayTests {
@@ -16,9 +16,15 @@ namespace Kurejito.ProviderTests.PayPal {
             throw new NotImplementedException();
         }
 
-        [Fact]
-        public void Test_Name() {
-	        
+        [Theory(Skip = "Got this working with extenal Excel data, but not a useful test yet.")]
+        [ExcelData(@"PayPal\PayPalDirectPaymentApiErrorCodes.xls", "select * from DirectPaymentApiErrorCodes")]
+        public void Purchase_Successful_Against_Sandbox(double code, string shortMessage, string longMessage, string correctiveAction, string kurejitoErrorCode) {
+            //https://cms.paypal.com/uk/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_testing_SBTestErrorConditions
+
+            Console.WriteLine(code);
+           
+//            var payPalNvpPaymentGateway = new PayPalDirectPaymentGateway(new HttpTransport(), PayPalEnvironment.NegativeTestAccountSandboxEnvironment());
+//            payPalNvpPaymentGateway.Purchase("REF", 100m, "GBP", new PaymentCard("BEN TAYLOR", "4716034283508634", new CardDate(10, 2015), "123", CardType.Visa)).Status.ShouldEqual(PaymentStatus.Ok);
         }
     }
 }
