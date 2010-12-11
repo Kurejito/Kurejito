acaa332e864361b679c54421fede5f57b4a162aa
Ben Taylor <ben.taylor.uk@gmail.com>
2010-12-11 17:38:06 +0000

    Updated error code mapping test to put PayPal error code in amount and parse expected PaymentStatus from the spreadsheet too.  At the moment they are all set to undefined so will fail.

--- src/Kurejito.ProviderTests/PayPal/PayPalPurchaseTests.cs
+++ src/Kurejito.ProviderTests/PayPal/PayPalPurchaseTests.cs
@@ -3,6 +3,7 @@ using Kurejito.Gateways.PayPal;
 using Kurejito.Gateways.PayPal.DirectPayment;
 using Kurejito.Payments;
 using Kurejito.Transport;
+using Should;
 using Xunit.Extensions;
 
 namespace Kurejito.ProviderTests.PayPal {
@@ -16,15 +17,16 @@ namespace Kurejito.ProviderTests.PayPal {
             throw new NotImplementedException();
         }
 
-        [Theory(Skip = "Got this working with extenal Excel data, but not a useful test yet.")]
+        [Theory]
         [ExcelData(@"PayPal\PayPalDirectPaymentApiErrorCodes.xls", "select * from DirectPaymentApiErrorCodes")]
-        public void Purchase_Successful_Against_Sandbox(double code, string shortMessage, string longMessage, string correctiveAction, string kurejitoErrorCode) {
+        public void PayPal_Error_Code_CorrectlyTranslates_To_Kurejito_Code(double code, string shortMessage, string longMessage, string correctiveAction, string kurejitoErrorCode) {
             //https://cms.paypal.com/uk/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_testing_SBTestErrorConditions
 
-            Console.WriteLine(code);
-           
-//            var payPalNvpPaymentGateway = new PayPalDirectPaymentGateway(new HttpTransport(), PayPalEnvironment.NegativeTestAccountSandboxEnvironment());
-//            payPalNvpPaymentGateway.Purchase("REF", 100m, "GBP", new PaymentCard("BEN TAYLOR", "4716034283508634", new CardDate(10, 2015), "123", CardType.Visa)).Status.ShouldEqual(PaymentStatus.Ok);
+            var payPalNvpPaymentGateway = new PayPalDirectPaymentGateway(new HttpTransport(), PayPalEnvironment.NegativeTestAccountSandboxEnvironment());
+            var amount = Convert.ToDecimal(code/100);
+            var expectedStatus = Enum.Parse(typeof (PaymentStatus), kurejitoErrorCode);
+            var paymentCard = new PaymentCard("BEN TAYLOR", "4716034283508634", new CardDate(10, 2015), "123", CardType.Visa);
+            payPalNvpPaymentGateway.Purchase("REF", amount, "GBP", paymentCard).Status.ShouldEqual(expectedStatus);
         }
     }
 }
