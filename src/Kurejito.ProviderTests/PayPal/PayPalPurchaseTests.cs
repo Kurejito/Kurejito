a60ab2c3970e4a9636f45cce9ba88d7697ccaa5d
Ben Taylor <ben.taylor.uk@gmail.com>
2010-12-12 16:06:40 +0000

    1. Moved PP sandbox test to ProviderTests and enabled.  2. Moved error codes xls to the unit test project in preparation for mocking the error code results.

--- src/Kurejito.ProviderTests/PayPal/PayPalPurchaseTests.cs
+++ src/Kurejito.ProviderTests/PayPal/PayPalPurchaseTests.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using Kurejito.Gateways.PayPal;
+﻿using Kurejito.Gateways.PayPal;
 using Kurejito.Gateways.PayPal.DirectPayment;
 using Kurejito.Payments;
 using Kurejito.Transport;
 using Should;
-using Xunit.Extensions;
+using Xunit;
 
 namespace Kurejito.ProviderTests.PayPal {
     public class PayPalPurchaseTests {
-
-        [Theory(Skip="Make this a unit test and fake the PayPal response as _way_ too slow")]
-        [ExcelData(@"PayPal\PayPalDirectPaymentApiErrorCodes.xls", "select * from DirectPaymentApiErrorCodes")]
-        public void PayPal_Error_Code_CorrectlyTranslates_To_Kurejito_Code(double code, string kurejitoErrorCode, string shortMessage, string longMessage, string correctiveAction) {
-            //https://cms.paypal.com/uk/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_testing_SBTestErrorConditions
-
+        [Fact]
+        public void Purchase_Successful_Against_Sandbox() {
             var payPalNvpPaymentGateway = new PayPalDirectPaymentGateway(new HttpTransport(), PayPalEnvironment.NegativeTestAccountSandboxEnvironment());
-            var amount = Convert.ToDecimal(code/100);
-            var expectedStatus = Enum.Parse(typeof (PaymentStatus), kurejitoErrorCode);
-            var paymentCard = new PaymentCard("BEN TAYLOR", "4716034283508634", new CardDate(10, 2015), "123", CardType.Visa);
-            payPalNvpPaymentGateway.Purchase("REF", amount, "GBP", paymentCard).Status.ShouldEqual(expectedStatus);
+            payPalNvpPaymentGateway.Purchase("REF", 100m, "GBP", new PaymentCard("BEN TAYLOR", "4716034283508634", new CardDate(10, 2015), "123", CardType.Visa)).Status.ShouldEqual(PaymentStatus.Ok);
         }
     }
 }
---
+++ src/Kurejito.Tests/Gateways/PayPal/DirectPayment/PayPalDirectPaymentTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Kurejito.Gateways.PayPal;
+using Kurejito.Gateways.PayPal.DirectPayment;
+using Kurejito.Payments;
+using Kurejito.Transport;
+using Should;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Kurejito.Tests.Gateways.PayPal.DirectPayment {
+    public class PayPalDirectPaymentTests {
+        
+
+        [Theory(Skip = "Make this a unit test and fake the PayPal response as _way_ too slow")]
+        [ExcelData(@"PayPal\PayPalDirectPaymentApiErrorCodes.xls", "select * from DirectPaymentApiErrorCodes")]
+        public void PayPal_Error_Code_CorrectlyTranslates_To_Kurejito_Code(double code, string kurejitoErrorCode, string shortMessage, string longMessage, string correctiveAction)
+        {
+            //https://cms.paypal.com/uk/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_testing_SBTestErrorConditions
+
+            var payPalNvpPaymentGateway = new PayPalDirectPaymentGateway(new HttpTransport(), PayPalEnvironment.NegativeTestAccountSandboxEnvironment());
+            var amount = Convert.ToDecimal(code / 100);
+            var expectedStatus = Enum.Parse(typeof(PaymentStatus), kurejitoErrorCode);
+            var paymentCard = new PaymentCard("BEN TAYLOR", "4716034283508634", new CardDate(10, 2015), "123", CardType.Visa);
+            payPalNvpPaymentGateway.Purchase("REF", amount, "GBP", paymentCard).Status.ShouldEqual(expectedStatus);
+        }
+    }
+}
