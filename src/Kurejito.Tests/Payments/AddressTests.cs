634e6473d4fe99e87d5be170c22228bb204c5a89
Ben Taylor <ben.taylor.uk@gmail.com>
2010-12-20 23:12:07 +0000

    Empty files for filling with Address work

---
+++ src/Kurejito.Tests/Payments/AddressTests.cs
@@ -0,0 +1,11 @@
+using Kurejito.Payments;
+using Xunit;
+
+namespace Kurejito.Tests.Payments {
+    public class AddressTests {
+        [Fact]
+        public void Test_Name() {
+            new Address();
+        }
+    }
+}
---
+++ src/Kurejito/Payments/Address.cs
@@ -0,0 +1,9 @@
+namespace Kurejito.Payments {
+    /// <summary>
+    /// International representation of a customers address.  Billing or shipping.
+    /// </summary>
+    public class Address {
+        //NOTE:  The initial address fields are based on the Active Merchant options hash fields:
+        //http://activemerchant.rubyforge.org/classes/ActiveMerchant/Billing/Gateway.html
+    }
+}
