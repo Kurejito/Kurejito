b6c31d0226e34a4f1646bd4dfbae74deb7918059
Ben Taylor <ben.taylor.uk@gmail.com>
2010-12-23 18:36:23 +0000

    1. Added initial properties to Address (based on PayPal NVP API).  2. Started my second gateway (Cardstream).

---
+++ src/Kurejito/Gateways/Cardstream/CardStreamGateway.cs
@@ -0,0 +1,13 @@
+namespace Kurejito.Gateways.Cardstream {
+    /// <summary>
+    /// Responsible for processing payments using the Cardstream service - http://www.cardstream.com
+    /// </summary>
+    /// <remarks>
+    /// Implementation based on the direct integration guide:
+    /// http://www.cardstream.com/downloads/Cardstream-HTTP-Integration-v8.2.pdf
+    /// </remarks>
+    public class CardStreamGateway {
+        //NOTE: BT implementing this along with PayPal in order to get better idea of API variance.
+        //Only picked Cardstream as their dev doc was easy to download!  No idea if any good :)
+    }
+}
--- src/Kurejito/Payments/Address.cs
+++ src/Kurejito/Payments/Address.cs
@@ -1,9 +1,75 @@
+using System;
+using Kurejito.Validation;
+
 namespace Kurejito.Payments {
     /// <summary>
     /// International representation of a customers address.  Billing or shipping.
     /// </summary>
-    public class Address {
-        //NOTE:  The initial address fields are based on the Active Merchant options hash fields:
-        //http://activemerchant.rubyforge.org/classes/ActiveMerchant/Billing/Gateway.html
+    public class Address : IValidate<Address> {
+        //NOTE: This initial set of properties are based on the PayPal NVP API fields.
+        //TODO: Enforce field lengths on Address to lowest common denominator in order to enable switching?
+
+        /// <summary>
+        /// Gets or sets the first name of the person at this address.
+        /// </summary>
+        /// <value>The first name.</value>
+        public string FirstName { get; set; }//NOTE: PayPal 25 single byte chars
+
+        /// <summary>
+        /// Gets or sets the last name of the person at this address.
+        /// </summary>
+        /// <value>The last name.</value>
+        
+        public string LastName { get; set; }//NOTE: PayPal 25 single byte chars
+        /// <summary>
+        /// Gets or sets the street.
+        /// </summary>
+        /// <value>The street.</value>
+        
+        public string Street { get; set; }//NOTE: PayPal 100 single byte chars
+        /// <summary>
+        /// Gets or sets the street2.
+        /// </summary>
+        /// <value>The street2.</value>
+        public string Street2 { get; set; }//NOTE: PayPal 100 single byte chars
+        
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>The city.</value>
+        public string City { get; set; }//NOTE: PayPal 40 single byte chars
+        
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>The state.</value>
+        public string State { get; set; }//NOTE: PayPal 40 single byte chars
+        
+        /// <summary>
+        /// Gets or sets the country code.
+        /// </summary>
+        /// <value>The country code.</value>
+        public string CountryCode { get; set; }//NOTE: PayPal 2 single byte chars
+        
+        /// <summary>
+        /// Gets or sets the postal code.
+        /// </summary>
+        /// <value>The postal code.</value>
+        public string PostalCode { get; set; }//NOTE: PayPal 20 single byte chars//No Zip here!
+        
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>The phone number.</value>
+        public string PhoneNumber { get; set; }//NOTE: PayPal 20 single byte chars
+        
+        /// <summary>
+        /// Determines whether this <see cref="Address"/> instance is considered valid.
+        /// </summary>
+        /// <param name="t">The t.</param>
+        /// <returns></returns>
+        public ValidationResult Validate(Address t) {
+            throw new NotImplementedException("Need to add validation for addresses based on CountryCode etc.");
+        }
     }
 }
